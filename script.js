11cadfeeaf53b4a1105c8c23909925ac62317916
Nicholas Mitchell <nickcode79@gmail.com>
2016-11-07 08:25:08 -0500

    styling and some javascript updates

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var getLatLong = function(address) {
                     iterate()
                 }, 500)
             } else {
-                alert('Geocode was not successful for the following reason: ' + status)
+                alert('Geocode failed because of this status code: ' + status)
             }
 
         })
@@ -53,7 +53,7 @@ var fetchData = function() {
     fetch('gunviolence.json')
         .then(function(response) {
             if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' + response.status)
+                console.log('Status Code: ' + response.status)
                 return
             }
             response.json().then(function(data) {
@@ -66,14 +66,14 @@ var fetchData = function() {
             })
         })
         .catch(function(err) {
-            console.log('Fetch Error :-S', err)
+            console.log('Fetch Error', err)
         })
 }
 
 var initMap = function() {
     geocoder = new google.maps.Geocoder()
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 5,
+        zoom: 4,
         center: new google.maps.LatLng(39.0997, -99.5786),
         mapTypeId: 'terrain'
     })
